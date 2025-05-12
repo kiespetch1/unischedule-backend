21a3b2ceda987b60671f3b5eb4152ae448ba0217
kiespetch <kiespetch@ya.ru>
2025-05-13 00:09:55 +0300

    добавлено обновление antiforgery токена на все сервисы

---
+++ src/Events/App/UniSchedule.Events.Api/Controllers/AccountController.cs
@@ -0,0 +1,26 @@
+﻿using System.Net;
+using Microsoft.AspNetCore.Mvc;
+using UniSchedule.Extensions.Attributes;
+
+namespace UniSchedule.Events.Api.Controllers;
+
+/// <summary>
+///     API для работы с авторизацией
+/// </summary>
+[ApiController]
+[Route("api/v1/[controller]")]
+public class AccountController : ControllerBase
+{
+    /// <summary>
+    ///     Обновление antiforgery токена
+    /// </summary>
+    /// <response code="200">Успешное обновление токена</response>
+    /// <response code="500">Непредвиденная ошибка</response>
+    [HttpPost("antiforgery")]
+    [ResponseStatusCodes(
+        HttpStatusCode.OK,
+        HttpStatusCode.InternalServerError)]
+    public void RefreshAntiforgery()
+    {
+    }
+}

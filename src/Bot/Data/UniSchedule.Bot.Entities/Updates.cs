589d96ab0784af930539883d2849de9a599b504d
Тараненко Данил <kiespetch@ya.ru>
2025-05-20 15:01:52 +0300

    добавлена сборка сущностей

---
+++ src/Bot/Data/UniSchedule.Bot.Entities/Updates.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json.Linq;
+
+namespace UniSchedule.Bot.Entities;
+
+public class Updates
+{
+    /// <summary>
+    ///     Тип события
+    /// </summary>
+    public string Type { get; set; }
+
+    /// <summary>
+    ///     Объект, инициировавший событие
+    ///     Структура объекта зависит от типа уведомления
+    /// </summary>
+    public JObject? Object { get; set; }
+
+    /// <summary>
+    ///     ID сообщества, в котором произошло событие
+    /// </summary>
+    public long GroupId { get; set; }
+}
